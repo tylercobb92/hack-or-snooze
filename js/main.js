209b972c7e746f6da5d4ccbf76cc307db2363f0e
tyler <tylercobb92@gmail.com>
2022-03-02 13:03:11 -0500

    select nav Favorites as const

--- js/main.js
+++ js/main.js
@@ -17,6 +17,8 @@ const $navLogin = $("#nav-login");
 const $navUserProfile = $("#nav-user-profile");
 const $navLogOut = $("#nav-logout");
 
+const $storiesLists = $(".stories-list");
+
 /** To make it easier for individual components to show just themselves, this
  * is a useful function that hides pretty much everything on the page. After
  * calling this, individual components can re-show just what they want.
